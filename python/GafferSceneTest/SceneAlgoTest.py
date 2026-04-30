murraystevenson/gaffer

commit aff9fde767d6f09e692c4097b17bba1a057c19a0

SceneAlgo : Return full history from attributeHistory()
Returning a non-truncated history fixes a few longstanding issues when interacting with Edit Scopes upstream of the first point in the graph where an attribute was created. These issues can be more apparent now that we're hosting render adaptors in our Scene Editors as they can be creating attributes at the last-minute, downstream of the last user-editable point of the node graph.

This also results in a slight change in behaviour when disabling edits (as seen in the change in AttributeInspectorTest.py). Previously when disabling an edit in "Source" mode, the history would no longer contain the disabled upstream plug, so subsequent disable actions would find the next available plug from the new history and disable it. Now we'd find the original plug, but not be able to disable it, as it is already disabled. The same occurs for editing, where we now find disabled attribute plugs on object source nodes upstream of Edit Scopes and present those to the user, whereas previously we'd only find those if they were enabled.

src/GafferScene/SceneAlgo.cpp
@@ -707,7 +707,8 @@ void addLocaliseAttributesPredecessors( const SceneAlgo::History::Predecessors &
 		{
 			continue;
 		}
-		if( auto p = attributeHistory( h.get(), destination->attributeName ) )
+		auto p = attributeHistory( h.get(), destination->attributeName );
+		if( p && p->attributeValue )
 		{
 			predecessor = p;
 			longestPath = sourcePath.size();
@@ -716,10 +717,45 @@ void addLocaliseAttributesPredecessors( const SceneAlgo::History::Predecessors &
 
 	if( !predecessor )
 	{
-		return;
+		if( !source.size() )
+		{
+			return;
+		}
+		// We didn't find a source with an attribute to localise. Add the first
+		// source as a predecessor so the history is not truncated.
+		predecessor = attributeHistory( source[0].get(), destination->attributeName );
 	}
 
+	assert( predecessor );
 	destination->predecessors.push_back( predecessor );
+}
+
+void addAttributeTweaksPredecessors( const AttributeTweaks *attributeTweaks, const SceneAlgo::History::Predecessors &source, SceneAlgo::AttributeHistory *destination )
+{
+	if( attributeTweaks->localisePlug()->getValue() )
+	{
+		for( const auto &tweak : TweakPlug::Range( *attributeTweaks->tweaksPlug() ) )
+		{
+			if(
+				tweak->namePlug()->getValue() == destination->attributeName.string() &&
+				tweak->enabledPlug()->getValue() &&
+				tweak->modePlug()->getValue() == TweakPlug::CreateIfMissing
+			)
+			{
+				// We don't want to include ancestor predecessors for a localised CreateIfMissing tweak.
+				// If the attribute exists on an ancestor the tweak shouldn't do anything, and if it
+				// doesn't exist, the tweak will create a new one. Either way, the ancestors are
+				// irrelevant to the history.
+				if( source.size() )
+				{
+					destination->predecessors.push_back( attributeHistory( source[0].get(), destination->attributeName ) );
+				}
+				return;
+			}
+		}
+	}
+
+	addLocaliseAttributesPredecessors( source, destination );
 }
 
 void addMergeScenesPredecessors( const MergeScenes *mergeScenes, const SceneAlgo::History::Predecessors &source, SceneAlgo::AttributeHistory *destination )
@@ -730,10 +766,22 @@ void addMergeScenesPredecessors( const MergeScenes *mergeScenes, const SceneAlgo
 	SceneAlgo::AttributeHistory::Ptr predecessor;
 	for( auto &h : source )
 	{
-		if( auto p = attributeHistory( h.get(), destination->attributeName ) )
+		auto p = attributeHistory( h.get(), destination->attributeName );
+		if( p && p->attributeValue )
 		{
 			predecessor = p;
 		}
+	}
+
+	if( !predecessor )
+	{
+		if( !source.size() )
+		{
+			return;
+		}
+		// We didn't find a source with an attribute to merge. Add the first
+		// source as a predecessor so the history is not truncated.
+		predecessor = attributeHistory( source[0].get(), destination->attributeName );
 	}
 
 	assert( predecessor );
@@ -875,11 +923,6 @@ SceneAlgo::AttributeHistory::Ptr SceneAlgo::attributeHistory( const SceneAlgo::H
 	ConstCompoundObjectPtr attributes = attributesHistory->scene->attributesPlug()->getValue();
 	ConstObjectPtr attributeValue = attributes->member<Object>( attribute );
 
-	if( !attributeValue )
-	{
-		return nullptr;
-	}
-
 	SceneAlgo::AttributeHistory::Ptr result = new AttributeHistory(
 		attributesHistory->scene, attributesHistory->context,
 		attribute, attributeValue
@@ -911,9 +954,9 @@ SceneAlgo::AttributeHistory::Ptr SceneAlgo::attributeHistory( const SceneAlgo::H
 		{
 			addMergeScenesPredecessors( mergeScenes, attributesHistory->predecessors, result.get() );
 		}
-		else if( runTimeCast<const AttributeTweaks>( node ) )
+		else if( auto attributeTweaks = runTimeCast<const AttributeTweaks>( node ) )
 		{
-			addLocaliseAttributesPredecessors( attributesHistory->predecessors, result.get() );
+			addAttributeTweaksPredecessors( attributeTweaks, attributesHistory->predecessors, result.get() );
 		}
 		else if( runTimeCast<const ShaderTweaks>( node ) )
 		{
@@ -1016,7 +1059,8 @@ ShaderTweaks *SceneAlgo::shaderTweaks( const ScenePlug *scene, const ScenePlug::
 	while( inheritancePath.size() )
 	{
 		History::ConstPtr h = history( scene->attributesPlug(), inheritancePath );
-		if( auto ah = attributeHistory( h.get(), attributeName ) )
+		auto ah = attributeHistory( h.get(), attributeName );
+		if( ah && ah->attributeValue )
 		{
 			return shaderTweaksWalk( ah.get() );
 		}

include/GafferScene/SceneAlgo.h
@@ -247,8 +247,7 @@ struct AttributeHistory : public History
 
 /// Filters `attributesHistory` and returns a history for the specific `attribute`.
 /// `attributesHistory` should have been obtained from a previous call to
-/// `history( scene->attributesPlug(), path )`. If the attribute doesn't exist then
-/// null is returned.
+/// `history( scene->attributesPlug(), path )`.
 GAFFERSCENE_API AttributeHistory::Ptr attributeHistory( const History *attributesHistory, const IECore::InternedString &attribute );
 
 /// Extends History to provide information on the history of a specific option.

python/GafferSceneUITest/AttributeInspectorTest.py
@@ -985,8 +985,8 @@ class AttributeInspectorTest( GafferUITest.TestCase ) :
 
 		inspection = self.__inspect( s["editScope2"]["out"], "/group/light", "gl:visualiser:scale", None )
 		self.assertFalse( inspection.canDisableEdit() )
-		self.assertEqual( inspection.nonDisableableReason(), "Source is in an EditScope. Change scope to editScope1 to disable." )
-		self.assertRaisesRegex( IECore.Exception, "Cannot disable edit : Source is in an EditScope. Change scope to editScope1 to disable.", inspection.disableEdit )
+		self.assertEqual( inspection.nonDisableableReason(), "light.visualiserAttributes.scale.enabled is not enabled." )
+		self.assertRaisesRegex( IECore.Exception, "Cannot disable edit : light.visualiserAttributes.scale.enabled is not enabled.", inspection.disableEdit )
 
 		inspection = self.__inspect( s["editScope2"]["out"], "/group/light", "gl:visualiser:scale", s["editScope2"] )
 		self.assertFalse( inspection.canDisableEdit() )

python/GafferSceneUITest/HistoryPathTest.py
@@ -442,14 +442,31 @@ class HistoryPathTest( GafferSceneTest.SceneTestCase ) :
 			historyPath = inspector.historyPath()
 
 		c = historyPath.children()
+		self.assertEqual( len( c ), 3 )
 
 		self.assertEqual( c[0].property( "name" ), str( c[0][-1] ) )
-		self.assertEqual( c[0].property( "history:node" ), s["openGLAttributes"] )
-		self.assertEqual( c[0].property( "history:value" ), 1536 )
-		self.assertEqual( c[0].property( "history:fallbackValue" ), 1024 )
+		self.assertEqual( c[0].property( "history:node" ), s["testLight"] )
+		self.assertEqual( c[0].property( "history:value" ), None )
+		self.assertEqual( c[0].property( "history:fallbackValue" ), 512 )
 		self.assertEqual( c[0].property( "history:operation" ), Gaffer.TweakPlug.Mode.Create )
-		self.assertEqual( c[0].property( "history:source" ), s["openGLAttributes"]["attributes"]["gl:visualiser:maxTextureResolution"] )
-		self.assertEqual( c[0].property( "history:editWarning" ), "Edits to \"gl:visualiser:maxTextureResolution\" may affect other locations in the scene." )
+		self.assertEqual( c[0].property( "history:source" ), s["testLight"]["visualiserAttributes"]["maxTextureResolution"] )
+		self.assertEqual( c[0].property( "history:editWarning" ), "" )
+
+		self.assertEqual( c[1].property( "name" ), str( c[1][-1] ) )
+		self.assertEqual( c[1].property( "history:node" ), s["group"] )
+		self.assertEqual( c[1].property( "history:value" ), None )
+		self.assertEqual( c[1].property( "history:fallbackValue" ), 512 )
+		self.assertEqual( c[1].property( "history:operation" ), None )
+		self.assertEqual( c[1].property( "history:source" ), None )
+		self.assertEqual( c[1].property( "history:editWarning" ), None )
+
+		self.assertEqual( c[2].property( "name" ), str( c[2][-1] ) )
+		self.assertEqual( c[2].property( "history:node" ), s["openGLAttributes"] )
+		self.assertEqual( c[2].property( "history:value" ), 1536 )
+		self.assertEqual( c[2].property( "history:fallbackValue" ), 1024 )
+		self.assertEqual( c[2].property( "history:operation" ), Gaffer.TweakPlug.Mode.Create )
+		self.assertEqual( c[2].property( "history:source" ), s["openGLAttributes"]["attributes"]["gl:visualiser:maxTextureResolution"] )
+		self.assertEqual( c[2].property( "history:editWarning" ), "Edits to \"gl:visualiser:maxTextureResolution\" may affect other locations in the scene." )
 
 		s["openGLAttributes"]["enabled"].setValue( False )
 
@@ -458,6 +475,7 @@ class HistoryPathTest( GafferSceneTest.SceneTestCase ) :
 			historyPath = inspector.historyPath()
 
 		c = historyPath.children()
+		self.assertEqual( len( c ), 3 )
 
 		self.assertEqual( c[0].property( "name" ), str( c[0][-1] ) )
 		self.assertEqual( c[0].property( "history:node" ), s["testLight"] )

src/GafferSceneUI/AttributeInspector.cpp
@@ -164,17 +164,7 @@ AttributeHistoryCache g_attributeHistoryCache(
 		assert( canceller == Context::current()->canceller() );
 		cost = 1;
 		SceneAlgo::History::ConstPtr attributesHistory = g_historyCache.get( key, canceller );
-		if( auto h = SceneAlgo::attributeHistory( attributesHistory.get(), key.attribute ) )
-		{
-			return h;
-		}
-		else
-		{
-			// The specific attribute doesn't exist. But we return the history for the
-			// whole CompoundObject so we get a chance to discover nodes that could
-			// _create_ the attribute.
-			return attributesHistory;
-		}
+		return SceneAlgo::attributeHistory( attributesHistory.get(), key.attribute );
 	},
 	// Max cost
 	1000,

commit 4754b0acfe5d630a6ad7c2fe49d54bc4b4197753

SceneAlgo : Fix performance regression in `[option|attribute]History()`
This was introduced in aff9fde, where we made changes to ensure that we returned a history even for missing options or attributes. The problem was that the fallback to `source[0]` meant that we were visiting the first source twice when the option or attribute didn't exist. And that source would be visiting its first source twice. And so on - leading to a big exponential explosion where depth N in the history would be visited 2^N times.

The solution is simple - just take `source[0]` in the main iteration over sources, replacing it if we find a better candidate. This gets us back to the performance we had before.

src/GafferScene/SceneAlgo.cpp
@@ -699,39 +699,29 @@ void addLocaliseAttributesPredecessors( const SceneAlgo::History::Predecessors &
 {
 	// No need to check if the node is filtered to this location.
 	// Filtered or unfiltered, it's all the same : the predecessor
-	// we want is the most local one. i.e. the one with the longest
+	// we want is the most local one. i.e. the one with a value and
+	// the longest path. If none have a value, then we take the longest
 	// path.
 
-	int longestPath = -1;
+	using Priority = std::tuple<bool, int>;
+	Priority highestPriority = { false, -1 };
 	SceneAlgo::AttributeHistory::Ptr predecessor;
 	for( auto &h : source )
 	{
-		const auto &sourcePath = h->context->get<ScenePlug::ScenePath>( ScenePlug::scenePathContextName );
-		if( (int)sourcePath.size() <= longestPath )
-		{
-			continue;
-		}
 		auto p = attributeHistory( h.get(), destination->attributeName );
-		if( p && p->attributeValue )
+		const auto &sourcePath = h->context->get<ScenePlug::ScenePath>( ScenePlug::scenePathContextName );
+		const Priority priority = { (bool)p->attributeValue, sourcePath.size() };
+		if( priority > highestPriority )
 		{
 			predecessor = p;
-			longestPath = sourcePath.size();
+			highestPriority = priority;
 		}
 	}
 
-	if( !predecessor )
+	if( predecessor )
 	{
-		if( !source.size() )
-		{
-			return;
-		}
-		// We didn't find a source with an attribute to localise. Add the first
-		// source as a predecessor so the history is not truncated.
-		predecessor = attributeHistory( source[0].get(), destination->attributeName );
+		destination->predecessors.push_back( predecessor );
 	}
-
-	assert( predecessor );
-	destination->predecessors.push_back( predecessor );
 }
 
 void addAttributeTweaksPredecessors( const AttributeTweaks *attributeTweaks, const SceneAlgo::History::Predecessors &source, SceneAlgo::AttributeHistory *destination )
@@ -771,25 +761,16 @@ void addMergeScenesPredecessors( const MergeScenes *mergeScenes, const SceneAlgo
 	for( auto &h : source )
 	{
 		auto p = attributeHistory( h.get(), destination->attributeName );
-		if( p && p->attributeValue )
+		if( !predecessor || p->attributeValue )
 		{
 			predecessor = p;
 		}
 	}
 
-	if( !predecessor )
+	if( predecessor )
 	{
-		if( !source.size() )
-		{
-			return;
-		}
-		// We didn't find a source with an attribute to merge. Add the first
-		// source as a predecessor so the history is not truncated.
-		predecessor = attributeHistory( source[0].get(), destination->attributeName );
+		destination->predecessors.push_back( predecessor );
 	}
-
-	assert( predecessor );
-	destination->predecessors.push_back( predecessor );
 }
 
 void addMergeScenesPredecessors( const MergeScenes *mergeScenes, const SceneAlgo::History::Predecessors &source, SceneAlgo::OptionHistory *destination )
@@ -801,25 +782,16 @@ void addMergeScenesPredecessors( const MergeScenes *mergeScenes, const SceneAlgo
 	for( auto &h : source )
 	{
 		auto p = optionHistory( h.get(), destination->optionName );
-		if( p && p->optionValue )
+		if( !predecessor || p->optionValue )
 		{
 			predecessor = p;
 		}
 	}
 
-	if( !predecessor )
+	if( predecessor )
 	{
-		if( !source.size() )
-		{
-			return;
-		}
-		// We didn't find a source with an option to merge. Add the first
-		// source as a predecessor so the history is not truncated.
-		predecessor = optionHistory( source[0].get(), destination->optionName );
+		destination->predecessors.push_back( predecessor );
 	}
-
-	assert( predecessor );
-	destination->predecessors.push_back( predecessor );
 }
 
 void addMergeScenesPredecessors( const MergeScenes *mergeScenes, const SceneAlgo::History::Predecessors &source, SceneAlgo::PrimitiveVariableHistory *destination )

python/GafferSceneTest/SceneAlgoTest.py
@@ -36,6 +36,7 @@
 
 import imath
 import inspect
+import time
 import unittest
 
 import IECore
@@ -1716,6 +1717,56 @@ class SceneAlgoTest( GafferSceneTest.SceneTestCase ) :
 		self.__assertAttributeHistory( attributeHistory, [ 0, 0 ], deleteAttributes["out"], "/light", "gl:visualiser:scale", None, 1 )
 		self.__assertAttributeHistory( attributeHistory, [ 0, 0, 0 ], deleteAttributes["in"], "/light", "gl:visualiser:scale", IECore.FloatData( 2.0 ), 1 )
 		self.__assertAttributeHistory( attributeHistory, [ 0, 0, 0, 0 ], testLight["out"], "/light", "gl:visualiser:scale", IECore.FloatData( 2.0 ), 0 )
+
+	def testAttributeHistoryScaling( self ) :
+
+		plane = GafferScene.Plane()
+
+		for nodeType in ( GafferScene.CustomAttributes, GafferScene.LocaliseAttributes, GafferScene.MergeScenes ) :
+
+			with self.subTest( nodeType = nodeType ) :
+
+				out = plane["out"]
+				nodes = []
+				for i in range( 0, 1000 ) :
+					nodes.append( nodeType() )
+					# Connect to `in` or `in[0]`.
+					next( GafferScene.ScenePlug.RecursiveInputRange( nodes[-1] ) ).setInput( out )
+					out = nodes[-1]["out"]
+
+				history = GafferScene.SceneAlgo.history( out["attributes"], "/plane" )
+				t = time.perf_counter()
+				GafferScene.SceneAlgo.attributeHistory( history, "scene:visible" )
+				# We actually expect this to return immediately - the limit of 1s
+				# is just in case we hit a CI machine under load. The bug this
+				# test case is protecting against meant that the test wouldn't return
+				# in any kind of practical time at all.
+				self.assertLess( time.perf_counter() - t, 1 )
+
+	def testOptionHistoryScaling( self ) :
+
+		plane = GafferScene.Plane()
+
+		for nodeType in ( GafferScene.CustomOptions, GafferScene.MergeScenes ) :
+
+			with self.subTest( nodeType = nodeType ) :
+
+				out = plane["out"]
+				nodes = []
+				for i in range( 0, 1000 ) :
+					nodes.append( nodeType() )
+					# Connect to `in` or `in[0]`.
+					next( GafferScene.ScenePlug.RecursiveInputRange( nodes[-1] ) ).setInput( out )
+					out = nodes[-1]["out"]
+
+				history = GafferScene.SceneAlgo.history( out["globals"] )
+				t = time.perf_counter()
+				GafferScene.SceneAlgo.optionHistory( history, "render:defaultRenderer" )
+				# We actually expect this to return immediately - the limit of 1s
+				# is just in case we hit a CI machine under load. The bug this
+				# test case is protecting against meant that the test wouldn't return
+				# in any kind of practical time at all.
+				self.assertLess( time.perf_counter() - t, 1 )
 
 	def testOptionHistory( self ) :
 